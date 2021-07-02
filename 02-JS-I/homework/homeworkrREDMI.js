eab32478df47c4e4d4fcf680ccf502e83989c3f6
GMG 98 <gonzua98@gmail.com>
2021-07-02 00:07:59 -0500

    homewrokjs1-js2

---
+++ 02-JS-I/homework/homeworkrREDMI.js
@@ -0,0 +1,12 @@
+*
+Variables = las variables son contenedores para almacenar datos "valores".
+
+*Strings = es una clase para manipular cadenas de caracteres.
+
+*Funciones(argumentos `return`) = es una funcion que sirve para devolverme "retorne"
+un valor o caracteristica asignada.
+
+*Declaraciones `if` = la funcion "if"
+sirve para declarar un bloque de varias operaciones en funcion a una expresion.
+
+*Valores booleanos(`true`, `false`) son aquellas variables que tienen un solo fin es verdadero o falso " si o no".
---
+++ 03-JS-II/homework/homeworkredmi2.js
@@ -0,0 +1,9 @@
+for = es un bucle que se utiliza para repetir una o mas instrucciones un determinado numero de veces.
+
+&&
+= se utilizan como un valor de y.
+
+||
+= se utiliza como un separador para un valor booleano.
+
+! = es un signo de interrogacion.
